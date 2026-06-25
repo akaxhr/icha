a7fad4a5e9610fb78d0c79685b28d5ee092c5e05
Akash R <imakash566@gmail.com>
2026-06-26 03:35:20 +0530

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -2,8 +2,7 @@ import { saveUserHistory } from "../lib/memory.js";
 import { handleAiReply } from "../lib/aiReply/index.js";
 import { handleCommands } from "../lib/router/commandRouter.js";
 import "../lib/commands/index.js";
-
-import { saveMessage } from "../lib/messages.js";
+import { logIncomingMessage } from "../lib/modules/messageLogger.js";
 import { getDisplayName, getGroupSettings } from "../lib/aliases.js";
 import { handleCallback } from "../lib/settingsUi.js";
 import {
@@ -88,29 +87,7 @@ if (adminAction) {
     const settings = await getGroupSettings(chatId);
 
     if (message.text || message.caption) {
-      await saveMessage({
-        chat_id: String(chatId),
-        chat_title:
-          message.chat.title ||
-          message.chat.first_name ||
-          message.chat.username ||
-          "Private Chat",
-        chat_type: message.chat.type,
-        user_id: userId,
-        username: displayName,
-        message_text: message.text || message.caption || "",
-        telegram_message_id: message.message_id,
-        reply_to_message_id: message.reply_to_message?.message_id || null,
-        reply_to_text:
-          message.reply_to_message?.text ||
-          message.reply_to_message?.caption ||
-          null,
-        reply_to_username:
-          message.reply_to_message?.from?.first_name ||
-          message.reply_to_message?.from?.username ||
-          null,
-        is_bot: false
-      });
+    await logIncomingMessage(message, userId, displayName);
     }
 
     if (await handleJoinLeave(message)) {
