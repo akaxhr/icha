8d8e988dbf8964450e6d1463db4159fb150dea9a
Akash R <imakash566@gmail.com>
2026-06-26 22:22:33 +0530

    Initialize state object for chat application

---
+++ public/store/state.js
@@ -0,0 +1,20 @@
+export const state = {
+
+    chats: [],
+
+    selectedChat: null,
+
+    messages: [],
+
+    groupInfo: null,
+
+    settings: null,
+
+    members: [],
+
+    media: [],
+
+    typing: false,
+
+    unread: {}
+};
