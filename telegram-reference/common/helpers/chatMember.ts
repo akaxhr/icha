9979973dfbc753ff7c9d12aae9903ce43a640ea7
Akash R <imakash566@gmail.com>
2026-08-30 10:06:32 +0530

    Delete telegram-reference directory

--- telegram-reference/common/helpers/chatMember.ts
+++
@@ -1,5 +0,0 @@
-import type { ApiChatMember } from '../../../api/types';
-
-export function hasRank(member?: ApiChatMember): boolean {
-  return Boolean(member && (member.rank || member.isOwner || member.isAdmin));
-}
