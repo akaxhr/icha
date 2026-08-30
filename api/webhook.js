fb2d07a7fb33414e0a70fe07cf4853677a6f1ed4
Akash R <imakash566@gmail.com>
2026-08-30 14:50:14 +0530

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -15,8 +15,6 @@ export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(200).send("Telegram bot is alive");
   }
-  await ensureUserClient();
-
   try {
     const update = req.body;
     const message = update.message;
