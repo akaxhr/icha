d01f379558bd9ca83ac1aacc18b2d7f0bc34ade4
Akash R <imakash566@gmail.com>
2026-09-04 16:11:42 +0530

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -2,12 +2,7 @@ import { generateWithFallback } from "../lib/ai.js";
 import { getUserHistory, saveUserHistory } from "../lib/memory.js";
 import { claimTelegramUpdate } from "../lib/supabase.js";
 import { sendTelegram } from "../lib/telegram.js";
-import {
-  playMusic,
-  skipMusic,
-  stopMusic,
-  getQueue,
-} from "../lib/musicWorker.js";
+import { musicCommand } from "../lib/music/index.js";
 import { saveMessage } from "../lib/messages.js";
 import { musicCommand } from "../lib/music/index.js";
 import { getDisplayName, getGroupSettings } from "../lib/aliases.js";
